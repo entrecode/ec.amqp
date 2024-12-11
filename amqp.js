e701b2967eb2807e5b066de700dfb3c68e9ba96f
Ruben Deyhle <deyhle@entrecode.de>
2024-12-11 15:58:37 +0100

    fix: support user/password with special characters

--- amqp.js
+++ amqp.js
@@ -21,8 +21,8 @@ function shuffleArray(array) {
   return array;
 }
 
-const connectionUser = config.get('amqp.user');
-const connectionPassword = config.get('amqp.password');
+const connectionUser = encodeURIComponent(config.get('amqp.user'));
+const connectionPassword = encodeURIComponent(config.get('amqp.password'));
 const useTLS = config.get('amqp.tls');
 const connectionURLs = shuffleArray(
   config.get('amqp.hosts').map((host) => `amqp${!!useTLS ? 's' : ''}://${connectionUser}:${connectionPassword}@${host}`),
